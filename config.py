5da65263aa366dafbaf92b6348f5721da4163ae0
NivethaThomasTnHeroine01 <nivethathomastnheroine01@gmail.com>
2025-03-19 17:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = os.environ.get("API_ID", "27066819")
-    API_HASH = os.environ.get("API_HASH", "823a68bafea9f46ab74f92656391746b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7644577147:AAFe6oKi8rLdsBnvxIP_amvZekjJhHRTcLY") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://ramsaranhero1:EMVwKybGR1lBF5DZ@cluster0.tst47tx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "ramsaranhero1")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6001367891').split()]
+    API_ID = int(environ.get("API_ID", "27066819"))
+    API_HASH = environ.get("API_HASH", "823a68bafea9f46ab74f92656391746b")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7644577147:AAFe6oKi8rLdsBnvxIP_amvZekjJhHRTcLY") 
+    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ramsaranhero1:EMVwKybGR1lBF5DZ@cluster0.tst47tx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "ramsaranhero1")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6001367891"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
